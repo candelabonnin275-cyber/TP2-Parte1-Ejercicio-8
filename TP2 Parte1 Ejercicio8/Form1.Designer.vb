e3a5da9fb191815b291c8a6b5925f8652934d399
Cande <Candelabonnin275@gmail.com>
2026-06-18 13:06:11 -0300

    Codigo hasta el ciclo mientras

--- TP2 Parte1 Ejercicio8/Form1.Designer.vb
+++ TP2 Parte1 Ejercicio8/Form1.Designer.vb
@@ -22,10 +22,53 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.cmdEjecutar = New Button()
+        Me.lblMostrar = New Label()
+        txtIngreso = New TextBox()
+        SuspendLayout()
+        ' 
+        ' cmdEjecutar
+        ' 
+        Me.cmdEjecutar.Font = New Font("Times New Roman", 12F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Me.cmdEjecutar.Location = New Point(278, 190)
+        Me.cmdEjecutar.Name = "cmdEjecutar"
+        Me.cmdEjecutar.Size = New Size(222, 56)
+        Me.cmdEjecutar.TabIndex = 0
+        Me.cmdEjecutar.Text = "Ejecutar"
+        Me.cmdEjecutar.UseVisualStyleBackColor = True
+        ' 
+        ' lblMostrar
+        ' 
+        Me.lblMostrar.AutoSize = True
+        Me.lblMostrar.Font = New Font("Times New Roman", 12F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Me.lblMostrar.Location = New Point(278, 152)
+        Me.lblMostrar.Name = "lblMostrar"
+        Me.lblMostrar.Size = New Size(0, 19)
+        Me.lblMostrar.TabIndex = 1
+        ' 
+        ' txtIngreso
+        ' 
+        txtIngreso.Location = New Point(278, 98)
+        txtIngreso.Name = "txtIngreso"
+        txtIngreso.Size = New Size(234, 23)
+        txtIngreso.TabIndex = 2
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
         AutoScaleMode = AutoScaleMode.Font
         ClientSize = New Size(800, 450)
+        Controls.Add(txtIngreso)
+        Controls.Add(Me.lblMostrar)
+        Controls.Add(Me.cmdEjecutar)
+        Name = "Form1"
         Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
     End Sub
+
+    Friend WithEvents cmdEjecutar As Button
+    Friend WithEvents lblMostrar As Label
+    Friend WithEvents txtIngreso As TextBox
 
 End Class
--- TP2 Parte1 Ejercicio8/Form1.vb
+++ TP2 Parte1 Ejercicio8/Form1.vb
@@ -1,3 +1,15 @@
 ﻿Public Class Form1
+    Dim Numero As Integer
+    Dim Contador As Integer
+    Dim Factorial As Integer
 
+    Private Sub cmdEjecutar_Click(sender As Object, e As EventArgs) Handles cmdEjecutar.Click
+        Numero = txtIngreso.Text
+        Contador = 0
+        Factorial = 1
+        Do While Contador < Numero
+            Contador = Contador + 1
+            Factorial = Factorial * Contador
+        Loop
+    End Sub
 End Class
